e07c5ab38bb0dd970c7a15482174df7e5b5bdba9
Eduardbrozmann21 <you@example.com>
2021-11-05 14:57:50 +0100

    Version 1

---
+++ Übung_Schaltjahre.py
@@ -0,0 +1,36 @@
+#Eduard Brozmann #05.11.2021 #
+
+import math
+jahr = 2000
+
+
+if (jahr % 4 == 0 ) and (jahr % 100 != 0) or (jahr % 400 == 0):     # == vergleicht != ungleich 
+     print(jahr, "ist ein schaltjahr")
+else:
+    print(jahr, "ist kein schaltjahr")
+
+
+#Erstellen Sie eine Liste mit allen Schaltjahren von 1800 bis 2100. Geben Sie die Liste anschließend aus.
+
+
+for i in range(1800,2101):
+    if (i % 4 == 0 ) and (i % 100 != 0) or (i % 400 == 0):
+        print(i,"ist ein schaltjahr")
+        
+else:
+       print(i,"ist kein schaltjahr")
+          
+    
+
+    
+     
+        
+   
+        
+
+
+
+    
+ 
+
+
